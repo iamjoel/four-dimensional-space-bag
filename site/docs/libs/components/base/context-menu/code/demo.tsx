389ca55293c266123b5d0050b86057b0b2a202ec
金伟强 <joel@metasecurelabs.io>
2023-02-11 22:18:20 +0800

    feat: context menu

---
+++ site/docs/libs/components/base/context-menu/code/demo.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useState } from 'react'
+import { Button } from '@arco-design/web-react'
+import ContextMenu from './index'
+
+const Demo: FC = ({
+}) => {
+  const [selected, setSelected] = useState()
+  return (
+    <div>
+      <ContextMenu
+        menus={[
+          {
+            key: 'create',
+            node: '创建'
+          },
+          {
+            key: 'copy',
+            node: (
+              <div className="flex justify-between">
+                  <span>复制</span>
+                  <span style={{color: '#77778C'}}>⌘C</span>
+                </div>
+            )
+          }
+        ]}
+        onAction={setSelected}
+      >
+        <Button type="primary">
+          右击点我
+        </Button>
+      </ContextMenu>
+      {selected && `刚刚点了：${selected}`}
+    </div>
+  )
+}
+
+export default React.memo(Demo)
---
+++ site/docs/libs/components/base/context-menu/code/index.tsx
@@ -0,0 +1,121 @@
+import React, { FC, ReactNode ,useEffect, useState, useRef, useLayoutEffect } from 'react'
+import { useBoolean, useClickAway } from 'ahooks'
+import ReactDOM from 'react-dom';
+
+import s from './style.module.css'
+
+type IMenus = {
+  key: string,
+  node: ReactNode
+}[]
+
+export interface IContextProps {
+  menus: IMenus
+  onAction: (action: string) => void,
+  children: JSX.Element
+  className?: string
+}
+const Context: FC<IContextProps> = ({
+  menus,
+  onAction,
+  children,
+  className=''
+}) => {
+  const [isShow, {setTrue: showMenu, setFalse: hideMenu}] = useBoolean(false)
+  const [pos, setPos] = useState({left: 0, top: 0})
+  const handleContextMenu = (e: any) => {
+    e.preventDefault()
+    showMenu()
+    setPos({
+      left: e.pageX,
+      top: e.pageY,
+    })
+  }
+
+  const menuWrapRef = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    if(!isShow || !menuWrapRef.current) {
+      return
+    }
+    const pageHeight = window.innerHeight;
+    const {height} = menuWrapRef.current.getBoundingClientRect()
+    if(pos.top + height > pageHeight) { // 下面间距不够，往上翻
+      setPos({
+        ...pos,
+        top: pos.top - height
+      })
+    }
+  }, [pos.left])
+
+  useClickAway(() => {
+    hideMenu()
+  }, menuWrapRef)
+
+  const handleAction = (action: string) => () => {
+    hideMenu()
+    onAction(action)
+  }
+
+  const Portal: any = usePortal(document.body)
+
+  return (
+    <div
+      className={`${s.wrap} ${className}`}
+      onContextMenu={handleContextMenu}
+    >
+      {children}
+      <Portal>
+        <div
+          className={s.menuWrap}
+          style={{
+            left: pos.left,
+            top: pos.top,
+            display: isShow ? 'block' : 'none'
+          }}
+          ref={menuWrapRef}
+        >
+          {menus.map(m => (
+            <div
+              key={m.key}
+              className={s.menuItem}
+              onClick={handleAction(m.key)}>
+              {m.node}
+            </div>
+          ))}
+        </div>
+      </Portal>
+    </div>
+  )
+}
+
+export default React.memo(Context)
+
+interface IPortalAPI {
+  render: ({children}: {children: JSX.Element}) => ReactNode
+  remove: () => void
+}
+
+const usePortal = (el: HTMLElement) => {
+  const [portal, setPortal] = React.useState<IPortalAPI>({
+    render: () => null,
+    remove: () => null,
+  });
+
+  const createPortal = React.useCallback(el => {
+    const Portal = ({ children }: {children: JSX.Element}) => ReactDOM.createPortal(children, el);
+    const remove = () => ReactDOM.unmountComponentAtNode(el);
+    return { render: Portal, remove };
+  }, []);
+
+  useEffect(() => {
+    if (el) portal.remove();
+    const newPortal = createPortal(el);
+    setPortal(newPortal);
+    return () => {
+      newPortal.remove();
+    }
+  }, [el]);
+
+  return portal.render;
+};
