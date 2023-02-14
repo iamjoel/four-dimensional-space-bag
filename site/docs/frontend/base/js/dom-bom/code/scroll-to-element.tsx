b941b0e4227ae0ba1d50c1a4bcece14e500d2e2a
金伟强 <joel@metasecurelabs.io>
2023-02-14 15:20:25 +0800

    feat: scroll hor

--- site/docs/frontend/base/js/dom-bom/code/scroll-to-element.tsx
+++ site/docs/frontend/base/js/dom-bom/code/scroll-to-element.tsx
@@ -1,14 +1,23 @@
 import React, { FC, useState, useEffect,  useRef } from 'react'
 import { Button } from "@arco-design/web-react"
+import scrollTo from '@site/src/utils/dom/scroll-to-elem'
 const ButtonGroup = Button.Group
 
 enum Color {
   red, green, blue
 }
 
+enum Dir {
+  HOR = 'hor',
+  VER = 'ver'
+}
+
 const ScrollToElem: FC = ({
 }) => {
+  
   const [current, setCurrent] = useState<Color>(Color.red)
+  const [dir, setDir] = useState<Dir>(Dir.VER)
+  const isVer = dir === Dir.VER
 
   const wrapElemRef = useRef<HTMLElement>(null)
   const redElemRef = useRef<HTMLElement>(null)
@@ -23,11 +32,24 @@ const ScrollToElem: FC = ({
 
   useEffect(() => {
     const scrollToElem = colorRefMap[current]
-    scrollTo(wrapElemRef.current, scrollToElem.current)
+    scrollTo(wrapElemRef.current, scrollToElem.current, isVer)
   }, [current])
 
   return (
     <div>
+      <div className='flex items-center mb-3'>
+        滚动方向:
+        <ButtonGroup className='ml-3'>
+          <Button 
+            type={dir === Dir.HOR ? 'primary' : 'outline'} shape='round'
+            onClick={() => setDir(Dir.HOR)}
+          >水平</Button>
+          <Button 
+            type={dir === Dir.VER ? 'primary' : 'outline'} shape='round'
+            onClick={() => setDir(Dir.VER)}
+          >竖直</Button>
+        </ButtonGroup>
+      </div>
       <div className='flex items-center mb-3'>
         滚动到:
         <ButtonGroup className='ml-3'>
@@ -45,10 +67,13 @@ const ScrollToElem: FC = ({
           >蓝</Button>
         </ButtonGroup>
       </div>
-      <div ref={wrapElemRef} className='overflow-y-scroll w-52 h-52'>
-        <div ref={redElemRef} className='h-52 bg-red-500'></div>
-        <div ref={greenElemRef} className='h-52 bg-green-500'></div>
-        <div ref={blueElemRef} className='h-52 bg-blue-500'></div>
+      <div ref={wrapElemRef} className={
+        `${isVer ? 'overflow-y-scroll' : 'flex overflow-x-scroll'}
+        w-52 h-52`
+      }>
+        <div ref={redElemRef} className={`${isVer ? 'h-52' : 'w-52 shrink-0'} bg-red-500`}></div>
+        <div ref={greenElemRef} className={`${isVer ? 'h-52' : 'w-52 shrink-0'} bg-green-500`}></div>
+        <div ref={blueElemRef} className={`${isVer ? 'h-52' : 'w-52 shrink-0'} bg-blue-500`}></div>
       </div>
     </div>
     
@@ -56,17 +81,3 @@ const ScrollToElem: FC = ({
 }
 
 export default React.memo(ScrollToElem)
-
-const scrollTo = (wrapElem, targetElem) => {
-  if(wrapElem && targetElem) {
-    // top: 距浏览器视口位置
-    const { top: wrapTop } = wrapElem.getBoundingClientRect()
-    const wrapScrollTop = wrapElem.scrollTop
-    const { top: childTop} = targetElem.getBoundingClientRect()
-    const scrollY = childTop - wrapTop + wrapScrollTop// 浏览器网上滚动了，子元素的top就小了滚动值
-    wrapElem.scrollTo({
-      top: scrollY,
-      behavior: 'smooth',
-    })
-  }
-}
