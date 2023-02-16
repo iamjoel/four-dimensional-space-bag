cc04833db06022451871659f8fd2bf4b0172b14d
金伟强 <joel@metasecurelabs.io>
2023-02-16 22:54:42 +0800

    feat: state demo

---
+++ site/docs/frontend/framework/react/api/code/use-state/demo.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+
+export const ChangeVarNotReRender = () => {
+  let value: any = {
+    current: 5
+  };
+  // (window as any).value = value
+  console.log(Math.random())
+
+  useEffect(() => {
+    const runId = setInterval(() => {
+      value.current = value.current + 1
+      console.log(value.current)
+      if(value.current > 30) {
+        clearInterval(runId)
+      }
+    }, 3000)
+    return () => {
+      clearInterval(runId)
+    }
+  }, [])
+  return (
+    <div>
+      value: {value.current}
+    </div>
+  )
+}
---
+++ site/docs/frontend/to-handle/resource.md
@@ -0,0 +1,2 @@
+# 待整理资源
+* [Plasmo](https://docs.plasmo.com/) 浏览器插件开发框架。支持 react。
