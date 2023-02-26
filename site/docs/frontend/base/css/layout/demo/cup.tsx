8a718fb26748d9b1f905b835a13fde7b4e0693a6
金伟强 <joel@metasecurelabs.io>
2023-02-26 23:15:39 +0800

    feat: cup

---
+++ site/docs/frontend/base/css/layout/demo/cup.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react'
+
+const Cup: FC = ({
+}) => {
+  return (
+    <div className="
+      flex flex-col
+      mx-auto
+      w-2/3 h-28
+      text-white
+    ">
+      <div className="h-6 shrink-0 bg-indigo-500 text-center ">header</div>
+      <div className="grow flex bg-red-500">
+        <div className="flex justify-center items-center w-12 shrink-0 bg-lime-500">left</div>
+        <div className="flex justify-center items-center  grow bg-orange-500">center</div>
+        <div className="flex  justify-center items-center w-12 shrink-0 bg-lime-500">right</div>
+      </div>
+      <div className="h-6 shrink-0 bg-indigo-500 text-center text-white">footer</div>
+    </div>
+  )
+}
+export default React.memo(Cup)
