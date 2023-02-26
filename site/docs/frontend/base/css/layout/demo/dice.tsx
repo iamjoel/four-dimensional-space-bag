45d452aca2a488b5f743d832ea3dc1f2494dd805
金伟强 <joel@metasecurelabs.io>
2023-02-26 22:21:51 +0800

    feat: dice

---
+++ site/docs/frontend/base/css/layout/demo/dice.tsx
@@ -0,0 +1,100 @@
+import React, { FC } from 'react'
+
+//骰子。 Flex
+const Dice: FC = ({
+}) => {
+  return (
+    <div className="flex space-x-3">
+      {/* 一 */}
+      <div className="
+        flex justify-center items-center
+        w-12 h-12 rounded
+        bg-indigo-500
+      ">
+        <div className="w-3 h-3 rounded-full bg-white"></div>
+      </div>
+
+      {/* 二 */}
+      <div className="
+        flex justify-between
+        w-12 h-12 rounded
+        p-1
+        bg-indigo-500
+      ">
+        <div className="w-3 h-3 rounded-full bg-white"></div>
+        <div className="self-end w-3 h-3 rounded-full bg-white"></div>
+      </div>
+
+      {/* 三 */}
+      <div className="
+        flex justify-between
+        w-12 h-12 rounded
+        p-1
+        bg-indigo-500
+      ">
+        <div className="w-3 h-3 rounded-full bg-white"></div>
+        <div className="self-center w-3 h-3 rounded-full bg-white"></div>
+        <div className="self-end w-3 h-3 rounded-full bg-white"></div>
+      </div>
+
+      {/* 四 */}
+      <div className="
+        flex flex-col justify-between
+        w-12 h-12 rounded
+        p-1
+        bg-indigo-500
+      ">
+        <div className="flex justify-between">
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+        </div>
+        <div className="flex justify-between">
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+        </div>
+      </div>
+
+      {/* 五 */}
+      <div className="
+        flex flex-col justify-between
+        w-12 h-12 rounded
+        p-1
+        bg-indigo-500
+      ">
+        <div className="flex justify-between">
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+        </div>
+        <div className="flex justify-center">
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+        </div>
+        <div className="flex justify-between">
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+        </div>
+      </div>
+
+      {/* 六 */}
+      <div className="
+        flex flex-col justify-between
+        w-12 h-12 rounded
+        p-1
+        bg-indigo-500
+      ">
+        <div className="flex justify-between">
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+        </div>
+        <div className="flex justify-between">
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+        </div>
+        <div className="flex justify-between">
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+          <div className="w-3 h-3 rounded-full bg-white"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default React.memo(Dice)
