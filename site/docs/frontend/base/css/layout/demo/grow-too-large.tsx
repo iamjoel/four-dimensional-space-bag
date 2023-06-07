ce0f51b6017ee01c429ab2ee46c5bf823dbf564e
Joel <iamjoel007@gmail.com>
2023-06-07 17:11:02 +0800

    feat: content too large break flex

---
+++ site/docs/frontend/base/css/layout/demo/grow-too-large.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+const GrowTooLargeBreakUI = ({
+}) => {
+  return (
+    <div className="flex h-40 text-white ">
+      <div className="shrink-0 h-full bg-indigo-500 px-1">Left</div>
+      <div className="h-full grow w-0 bg-indigo-400">
+        <div className="white  overflow-hidden text-ellipsis whitespace-nowrap">adasdasdasdadasdasdasdasdasadasdasdasdadasdasadasdasdasdadasdasdasdasdasadasdasdasdadasdas large part</div>
+      </div>
+    </div>
+  )
+}
+
+export default React.memo(GrowTooLargeBreakUI)
