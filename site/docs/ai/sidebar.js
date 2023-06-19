3299755b440f99b86961159cc8d4e69e0df28de6
Joel <iamjoel007@gmail.com>
2023-06-19 15:13:52 +0800

    feat: build app

--- site/docs/ai/sidebar.js
+++ site/docs/ai/sidebar.js
@@ -8,8 +8,30 @@ const config = [
   },
   {
     type: 'doc',
-    label: '提示词',
-    id: `${PREFIX}/prompt/overview`,
+    label: 'LLM 模型概览',
+    id: `${PREFIX}/model/llm`,
+  },
+  {
+    type: 'category',
+    label: '构建应用',
+    collapsed: false,
+    items: [
+      {
+        type: 'doc',
+        label: '概览',
+        id: `${PREFIX}/build-app/overview`,
+      },
+      {
+        type: 'doc',
+        label: '提示词',
+        id: `${PREFIX}/build-app/prompt/overview`,
+      },
+      {
+        type: 'doc',
+        label: 'Flow',
+        id: `${PREFIX}/build-app/flow/overview`,
+      },
+    ]
   },
   {
     type: 'doc',
