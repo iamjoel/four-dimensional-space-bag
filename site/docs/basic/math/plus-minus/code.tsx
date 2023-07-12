8b020f53c7820d7b4cb5f7e94386b76f2b0c294e
Joel <iamjoel007@gmail.com>
2023-07-12 17:59:21 +0800

    feat: finisha

--- site/docs/basic/math/plus-minus/code.tsx
+++ site/docs/basic/math/plus-minus/code.tsx
@@ -10,7 +10,7 @@ type RES = {
   op: string
   res: number
 }
-
+// 到50题结束。实时的总时间；错误放出来；
 // 20 以内加减法
 function genRandomNumber(max = 10) {
   const res = Math.round((Math.random()) * max)
@@ -93,11 +93,20 @@ function Keyboard({
   )
 }
 const PlusMinus: FC = () => {
-  const total = 100
+  const [isStart, setIsStart, getIsStart] = useGetState(false)
+  const total = 50
   const [completedNum, setCompletedNum] = useState(0)
-  const EACH_TIME = 5
+  const [isCompleted, setIsCompleted, getIsCompleted] = useGetState(false)
+  const [seconds, setSeconds, getSeconds] = useGetState(0)
+  const completedTime = (() => {
+    const minutes = Math.floor(seconds / 60)
+    const resSeconds = seconds % 60
+    return `${minutes > 0 ? `${minutes}分` : ''}${resSeconds}秒`
+  })()
+  const EACH_TIME = 3
   const [countDown, setCountDown, getCountDown] = useGetState(EACH_TIME)
   const [current, setCurrent] = useState<RES>({})
+  const [errorList, setErrorList] = useState([])
 
   const genNext = () => {
     setCompletedNum(completedNum + 1)
@@ -107,8 +116,13 @@ const PlusMinus: FC = () => {
   const [res, setRes] = useState('')
   const handleOnEnter = () => {
     if (Number.parseInt(res) !== current.res) {
+      setErrorList([...errorList, `${current.left} ${current.op} ${current.right} = ${res}`])
       Message.error('错了')
       setRes('')
+      return
+    }
+    if (completedNum === total) {
+      setIsCompleted(true)
       return
     }
     setRes('')
@@ -118,6 +132,12 @@ const PlusMinus: FC = () => {
   useEffect(() => {
     genNext()
     const runId = setInterval(() => {
+      if (!getIsStart())
+        return
+
+      if (!getIsCompleted())
+        setSeconds(getSeconds() + 1)
+
       const nextCountDown = getCountDown() - 1
       if (nextCountDown >= 0)
         setCountDown(nextCountDown)
@@ -127,10 +147,41 @@ const PlusMinus: FC = () => {
     }
   }, [])
 
+  if (!isStart) {
+    return (
+      <div className='p-3 shadow-lg'>
+        <div className='mb-3'>要求：50道，一道不超过 3 秒。</div>
+        <Button size="large" type='primary' onClick={() => setIsStart(true)}>开始</Button>
+      </div>
+    )
+  }
+
+  if (isCompleted) {
+    return (<div className='p-3 shadow-lg text-base'>
+      <div className='flex space-x-2'>
+        <div>用时间：{completedTime}</div>
+        <div>错误数：{errorList.length}</div>
+      </div>
+      {errorList.length > 0 && (
+        <div className='space-y-1'>
+          <div className='mt-2 mb-1'>错误题目:</div>
+          {errorList.map((item, index) => (
+            <div key={item}>{index + 1}. {item}</div>
+          ))}
+        </div>
+      )}
+
+    </div>)
+  }
+
   return (
-    <div className='mx-auto'>
+    <div className='p-3 shadow-lg'>
       <Progress percent={countDown / EACH_TIME * 100} showText={false} />
       <div className='text-[#f00] h-[22px]'>{countDown === 0 ? '超时了' : ' '}</div>
+      <div className='mt-3 mb-1 flex justify-between'>
+        <div><span className='text-[#0f0]'>{completedNum}</span> / {total}</div>
+        {/* <Button type='primary' onClick={genNext}>下一题</Button> */}
+      </div>
       <div className='flex text-[80px]'>
         {current.left} {current.op} {current.right} = {res}
       </div>
@@ -140,11 +191,7 @@ const PlusMinus: FC = () => {
         onChange={setRes}
         onEnter={handleOnEnter}
       />
-      <div className='mt-3 pb-10 flex justify-between'>
-        <div><span className='text-[#0f0]'>{completedNum}</span> / {total}</div>
-        {/* <Button type='primary' onClick={genNext}>下一题</Button> */}
-      </div>
-    </div>
+    </div >
   )
 }
 export default React.memo(PlusMinus)
